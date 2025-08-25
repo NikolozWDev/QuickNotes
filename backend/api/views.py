b85e98a13f85ad8fc28670a3112fd9a041570509
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-08-26 00:05:16 +0400

    added frontend tools, working with JWT

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .models import Note
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
@@ -9,3 +10,27 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class CreateNoteView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class DeleteNoteView(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <p className="bg-green-600 text-white text-[18px] font-bold p-[10px]">homepage</p>
+        </div>
+    )
+}
+export default HomePage
