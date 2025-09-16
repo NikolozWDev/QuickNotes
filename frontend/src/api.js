68c1287e7d9d4e5fc4b3d0f23cfe394097384ff1
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-09-16 21:05:42 +0400

    added backend url into env and api js file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const apiURL = "/choreo-apis/quicknotes/backend/v1"
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+    baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 })
 
 api.interceptors.request.use(
